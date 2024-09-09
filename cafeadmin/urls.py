ef5960bb28addbb97638811aa1ab48e78512b858
sarahwonago <sarahshaaleh@gmail.com>
2024-09-09 14:08:25 +0300

    category detail apiview

--- cafeadmin/urls.py
+++ cafeadmin/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
-from .views import (AdminHome, ListCreateCategory, update_category, get_categories,delete_category,
-                    fooditem_create, get_fooditems, fooditem_detail
+from .views import (AdminHome, ListCreateCategory, DetailUpdateDeleteCategory,
                     )
 
 urlpatterns = [
     path("dashboard/", AdminHome.as_view(), name="admin-home"),
-    path("categories/list/", ListCreateCategory.as_view(), name="list_create_category"),
-    path("categories/", get_categories, name="get_categories"),
-    path("categories/update/<uuid:pk>/", update_category, name="update_category"),
-    path("categories/delete/<uuid:pk>/", delete_category, name="delete_category"),
-    path("categories/fooditem/create/", fooditem_create, name="create_fooditem"),
-    path("categories/fooditems/", get_fooditems, name="get_fooditems"),
-    path("categories/fooditem/detail/<uuid:pk>/", fooditem_detail, name="detail_fooditem"),
+    path("categories/", ListCreateCategory.as_view(), name="category-list-create"),
+    path("category/<uuid:pk>/", DetailUpdateDeleteCategory.as_view(), name="category-detail"),
 ]
--- cafeadmin/views.py
+++ cafeadmin/views.py
@@ -116,155 +116,117 @@ class ListCreateCategory(APIView):
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated, IsAdmin])
-def get_categories(request):
+class DetailUpdateDeleteCategory(APIView):
     """
-    Retrieve a list of all categories.
+    View to handle retrieving, updating, and deleting a single category.
 
     Only accessible to admin users.
 
-    Returns:
-        Response: A JSON response containing a list of all categories.
+    Methods:
+        get: Retrieve a single category by its ID.
+        put: Update a category.
+        delete: Delete a category.
     """
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
 
-    return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated, IsAdmin])
-def update_category(request, pk):
-    """
-    Update an existing category by its UUID.
+    permission_classes = [IsAuthenticated, IsAdmin]
+    serializer_class = CategorySerializer
 
-    Only accessible to admin users.
+    def get_object(self, pk):
+        """
+        Utility function for fetching a single category by ID.
 
-    Args:
-        request (Request): The HTTP request containing the updated category data.
-        pk (UUID): The UUID of the category to update.
+        Args:
+            pk (UUID): The primary key of the category to retrieve.
 
-    Returns:
-        Response: A JSON response containing the updated category or errors.
-    """
-    try:
-        category = Category.objects.get(pk=pk)
+        Returns:
+            category (Category): The fetched category.
+        """
 
-    except Category.DoesNotExist:
-        ct = {
-            "detail": "Category not found."
-        }
-        return Response(ct, status=status.HTTP_404_NOT_FOUND)
-    
-    serializer = CategorySerializer(category, data=request.data)
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            category = None
+        
+        return category
 
-    if serializer.is_valid():
-        serializer.save()
+    def get(self, request, pk=None):
+        """
+        Handle GET requests for retrieving a single category by ID.
+
+        Args:
+            pk (UUID): The primary key of the category to retrieve.
+            request (Request): The Http request
+
+        Returns:
+            Response: A response containing the category data.
+        """
+        
+        category = self.get_object(pk=pk)
+
+        if not category:
+            response = {
+                "detail": "Category not found."
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CategorySerializer(category)
+
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk=None):
+        """
+        Handle PUT requests for updating an existing category by ID.
 
-@api_view(['DELETE'])
-@permission_classes([IsAuthenticated, IsAdmin])
-def delete_category(request, pk):
-    """
-    Delete an existing category by its UUID.
+        Args:
+            pk (UUID): The primary key of the category to update.
+            request (Request): The Http request
 
-    Only accessible to admin users.
+        Returns:
+            Response: A response containing the updated category data or validation errors.
+        """
 
-    Args:
-        pk (UUID): The UUID of the category to delete.
+        data = request.data
 
-    Returns:
-        Response: A JSON response confirming the deletion.
-    """
+        category = self.get_object(pk=pk)
 
-    try:
-        category = Category.objects.get(pk=pk)
-
-    except Category.DoesNotExist:
-        ct = {
-            "detail": "Category not found."
-        }
-        return Response(ct, status=status.HTTP_404_NOT_FOUND)
-    
-    category.delete()
-
-    return Response({"detail":"Category deleted successfully."},status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated, IsAdmin])
-def fooditem_create(request):
-    """
-    Creates a new fooditem.
-
-    Only accessible to admin users.
-
-    Args:
-        request(Request): The HTTP request containing the fooditem data.
-
-    Returns:
-        Response: A JSON response containing the newly created fooditem or errors.
-    """
-
-    serializer = FoodItemSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated, IsAdmin])
-def get_fooditems(request):
-    """
-    Fetches all fooditems under a category.
-
-    Only accessible to admin users.
-
-    Returns:
-        Response: A JSON response containing all the fooditems under a category or errors.  
-    """
-    fooditems = FoodItem.objects.all()
-    serializer = FoodItemSerializer(fooditems, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsAuthenticated, IsAdmin])
-def fooditem_detail(request, pk):
-    """
-    Retrieves, update or delete an existing fooditem.
-
-    Only accessible to admin users.
-
-    Args:
-        pk (UUID): The UUID of the fooditem to retrieve, update or delete.
-
-    Returns:
-        Response: A JSON response confirming the deletion, updation, fetchingor errors.
-    """
-
-    try:
-        fooditem = FoodItem.objects.get(pk=pk)
-
-    except FoodItem.DoesNotExist:
-        return Response({"detail": "Fooditem not found."}, status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = FoodItem(fooditem)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    elif request.method == 'PUT':
-        serializer = FoodItem(fooditem, data=request.data)
+        if not category:
+            response = {
+                "detail": "Category not found."
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = CategorySerializer(data=data, instance=category)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk=None):
+        """
+        Handle DELETE requests for deleting a category by ID.
 
-    elif request.method == 'DELETE':
-        fooditem.delete()
-        return Response({"detail": "Food item deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+        Args:
+            pk (UUID): The primary key of the category to delete.
+            request (Request): The Http request
+
+        Returns:
+            Response: A response confirming deletion.
+        """
+        category = self.get_object(pk=pk)
+
+        if not category:
+            response = {
+                "detail": "Category not found."
+            }
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        
+        category.delete()
+
+        response = {
+            "detail":"category deleted successfully"
+        }
+       
+        return Response(response, status=status.HTTP_204_NO_CONTENT)
